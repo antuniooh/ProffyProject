95cce5042c0481720fca45bc2688606def0d5e0c
Antonio Muniz <antonio.gustavo_@live.com>
2021-02-28 23:40:20 -0300

    Added new type of fields

--- web-app/src/pages/TeacherForm/index.tsx
+++ web-app/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,72 @@
 import React from 'react';
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from "../../assets/images/icons/warning.svg";
+
+import './styles.css';
 
 function TeacherForm(){
     return(
         <div id="page-teacher-form" className="container">
-           <PageHeader title="Que incrível que você quer dar aulas.">
-               
-               
+           <PageHeader 
+            title="Que incrível que você quer dar aulas."
+            description="O primeiro passo é preencher esse formulário de inscrição"
+           >
             </PageHeader>
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="name" label="Nome completo"></Input>
+                    <Input name="avatar" label="Avatar"></Input>
+                    <Input name="whatsapp" label="WhatsApp"></Input>
+                    <Textarea name="bio" label="Biografia"></Textarea>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Select 
+                    name="subject" 
+                    label="Matéria"
+                    options={[
+                        { value: 'Artes', label: 'Artes'},
+                        { value: 'Biologia', label: 'Biologia'},
+                        { value: 'Ciências', label: 'Ciências'},
+                        { value: 'Educação Física', label: 'Educação Física'},
+                        { value: 'Física', label: 'Física'},
+                        { value: 'Geografia', label: 'Geografia'},
+                        { value: 'Matemática', label: 'Matemática'},
+                        { value: 'Português', label: 'Português'},
+                        { value: 'Química', label: 'Química'},
+                        { value: 'História', label: 'História'}
+                    ]}
+                    />
+                    <Input name="cost" label="Custo da sua hora por aula"></Input>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Horários disponíveis
+                    <button type="button">
+                        + Novo horário
+                    </button>
+                    </legend>
+                    
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso Importante"/>
+                        Importante <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     )
 }
